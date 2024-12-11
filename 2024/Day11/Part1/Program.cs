48cf21a8fffc83184eca6cbaf7ac0bb86e4c25db
David Fink <itzcrazyzockt@gmail.com>
2024-12-11 14:28:02 +0100

    Day 11: Part1/Part2 completed

---
+++ 2024/Day11/Part1/Program.cs
@@ -0,0 +1,59 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+//===============================================================
+// PARSING
+//===============================================================
+
+List<long> stones = new List<long>();
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(' ');
+
+    for (int i = 0; i < parts.Length; i++)
+    {
+        stones.Add(long.Parse(parts[i]));
+    }
+}
+
+//===============================================================
+// LOGIC
+//===============================================================
+
+int blinks = 25;
+
+for (int i = 0; i < blinks; i++)
+{
+    for (int j = stones.Count - 1; j >= 0; j--)
+    {
+        if (stones[j] == 0)
+        {
+            stones[j] = 1;
+        }
+        else if (HasEvenAmountOfDigits(stones[j]))
+        {
+            string number = stones[j].ToString();
+            stones[j] = long.Parse(number.Substring(0, number.Length / 2));
+            stones.Insert(j + 1, long.Parse(number.Substring(number.Length / 2)));
+        }
+        else
+        {
+            stones[j] *= 2024;
+        }
+    }
+}
+
+Console.WriteLine("Result: " + stones.Count);
+
+bool HasEvenAmountOfDigits(long number)
+{
+    int count = 0;
+
+    while (number != 0)
+    {
+        number /= 10;
+        count++;
+    }
+
+    return count % 2 == 0;
+}
---
+++ 2024/Day11/Part2/Program.cs
@@ -0,0 +1,72 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+//===============================================================
+// PARSING
+//===============================================================
+
+Dictionary<long, long> stoneAppearances = new Dictionary<long, long>();
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(' ');
+
+    for (int i = 0; i < parts.Length; i++)
+    {
+        stoneAppearances.Add(int.Parse(parts[i]), 1);
+    }
+}
+
+//===============================================================
+// LOGIC
+//===============================================================
+
+int blinks = 75;
+
+for (int i = 0; i < blinks; i++)
+{
+    Dictionary<long, long> newStoneAppearances = new Dictionary<long, long>();
+
+    foreach (var stoneAppearance in stoneAppearances)
+    {
+        if (stoneAppearance.Value == 0)
+        {
+            continue;
+        }
+
+        if (stoneAppearance.Key == 0)
+        {
+            newStoneAppearances[1] = newStoneAppearances.GetValueOrDefault(1) + stoneAppearance.Value;
+        }
+        else if (HasEvenAmountOfDigits(stoneAppearance.Key))
+        {
+            string number = stoneAppearance.Key.ToString();
+
+            long firstHalf = long.Parse(number.Substring(0, number.Length / 2));
+            long secondHalf = long.Parse(number.Substring(number.Length / 2));
+
+            newStoneAppearances[firstHalf] = newStoneAppearances.GetValueOrDefault(firstHalf) + stoneAppearance.Value;
+            newStoneAppearances[secondHalf] = newStoneAppearances.GetValueOrDefault(secondHalf) + stoneAppearance.Value;
+        }
+        else
+        {
+            newStoneAppearances[stoneAppearance.Key * 2024] = newStoneAppearances.GetValueOrDefault(stoneAppearance.Key * 2024) + stoneAppearance.Value;
+        }
+    }
+
+    stoneAppearances = new Dictionary<long, long>(newStoneAppearances);
+}
+
+Console.WriteLine("Result: " + stoneAppearances.Sum(x => x.Value));
+
+bool HasEvenAmountOfDigits(long number)
+{
+    int count = 0;
+
+    while (number != 0)
+    {
+        number /= 10;
+        count++;
+    }
+
+    return count % 2 == 0;
+}
