26f867e5888b5bae41cfd76923d196678dfba748
David Fink <itzcrazyzockt@gmail.com>
2024-12-13 13:52:21 +0100

    Day 13: Part1/Part2 completed

---
+++ 2024/Day13/Part1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("../input.txt");
+List<ClawMachine> clawMachines = new List<ClawMachine>();
+
+//===============================================================
+// PARSING
+//===============================================================
+
+for (int i = 0; i < lines.Length; i+=4)
+{
+    Regex regexButtons = new Regex(@"X\+(\d+), Y\+(\d+)");
+    Regex regexPrize = new Regex(@"X=(\d+),\sY=(\d+)");
+    
+    List<Match> matches =
+    [
+        regexButtons.Match(lines[i]),
+        regexButtons.Match(lines[i + 1]),
+        regexPrize.Match(lines[i + 2]),
+    ];
+
+    List<Vector2> positions = new List<Vector2>();
+
+    foreach (Match match in matches)
+    {
+        if (match.Success)
+        {
+            positions.Add(new Vector2(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)));
+        }
+    }
+
+    if (positions.Count == 3)
+    {
+        clawMachines.Add(new ClawMachine(positions[0], positions[1], positions[2]));
+    }
+}
+
+//===============================================================
+// LOGIC
+//===============================================================
+
+int result = 0;
+
+foreach (ClawMachine clawMachine in clawMachines)
+{
+    int minimumCost = int.MaxValue;
+    for(int i = 1; i <= 100; i++)
+    {
+        for(int j = 1; j <= 100; j++)
+        {
+            int combinedPositionX = (int)(clawMachine.buttonA.X * i + clawMachine.buttonB.X * j);
+            int combinedPositionY = (int)(clawMachine.buttonA.Y * i + clawMachine.buttonB.Y * j);
+
+            if (combinedPositionX == clawMachine.prizePosition.X && combinedPositionY == clawMachine.prizePosition.Y)
+            {
+                minimumCost = i * 3 + j;
+            }
+        }
+    }
+
+    if (minimumCost != int.MaxValue)
+    {
+        result += minimumCost;
+    }
+}
+
+Console.WriteLine("Result: " + result);
+
+class ClawMachine
+{
+    public Vector2 buttonA;
+    public Vector2 buttonB;
+    public Vector2 prizePosition;
+
+    public ClawMachine(Vector2 buttonA, Vector2 buttonB, Vector2 prizePosition)
+    {
+        this.buttonA = buttonA;
+        this.buttonB = buttonB;
+        this.prizePosition = prizePosition;
+    }
+}
---
+++ 2024/Day13/Part2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("../input.txt");
+List<ClawMachine> clawMachines = new List<ClawMachine>();
+
+//===============================================================
+// PARSING
+//===============================================================
+
+for (int i = 0; i < lines.Length; i+=4)
+{
+    Regex regexButtons = new Regex(@"X\+(\d+), Y\+(\d+)");
+    Regex regexPrize = new Regex(@"X=(\d+),\sY=(\d+)");
+    
+    List<Match> matches =
+    [
+        regexButtons.Match(lines[i]),
+        regexButtons.Match(lines[i + 1]),
+        regexPrize.Match(lines[i + 2]),
+    ];
+
+    List<Vector2d> positions = new List<Vector2d>();
+
+    foreach (Match match in matches)
+    {
+        if (match.Success)
+        {
+            positions.Add(new Vector2d(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)));
+        }
+    }
+
+    if (positions.Count == 3)
+    {
+        clawMachines.Add(new ClawMachine(positions[0], positions[1], positions[2]));
+    }
+}
+
+//===============================================================
+// LOGIC
+//===============================================================
+
+long result = 0;
+
+foreach (ClawMachine clawMachine in clawMachines)
+{
+    // Solved by calculating the following equations by elimination
+    // First equation firstA * x + secondA * y = thirdA
+    // Second equation firstB * x + secondB * y = thirdB
+    // x is the amount of times button A has to be pressed, y is the amount of times button B has to be pressed
+
+    // First make the coefficients of y in both equations equal by multiplying the equations with the other equation's y coefficient
+    double firstA = clawMachine.buttonA.X * clawMachine.buttonB.Y;
+    double secondA = clawMachine.buttonB.X * clawMachine.buttonB.Y;
+    double thirdA = clawMachine.prizePosition.X * clawMachine.buttonB.Y;
+    
+    double firstB = clawMachine.buttonA.Y * clawMachine.buttonB.X;
+    double secondB = clawMachine.buttonB.Y * clawMachine.buttonB.X;
+    double thirdB = clawMachine.prizePosition.Y * clawMachine.buttonB.X;
+
+    // Substract the second equation from the first one to eliminate the y factor and only have x as the only unknown variable left
+    double first = firstA - firstB;
+    double second = secondA - secondB;
+    double third = thirdA - thirdB;
+
+    // Calculate x
+    double solutionX = third / first;
+
+    // Calculate y by substituting x into the first equation
+    double solutionY = (thirdA - (firstA * solutionX)) / secondA;
+
+    // If the solutions are whole numbers its possible to reach the prize position
+    if (solutionX == Math.Floor(solutionX) && solutionY == Math.Floor(solutionY))
+    {
+        result += (long)solutionX * 3 + (long)solutionY;
+    }
+}
+
+Console.WriteLine("Result: " + result);
+
+class ClawMachine
+{
+    public Vector2d buttonA;
+    public Vector2d buttonB;
+    public Vector2d prizePosition;
+
+    public ClawMachine(Vector2d buttonA, Vector2d buttonB, Vector2d prizePosition)
+    {
+        this.buttonA = buttonA;
+        this.buttonB = buttonB;
+        this.prizePosition = new Vector2d(prizePosition.X + 10000000000000, prizePosition.Y + 10000000000000);
+    }
+}
+
+class Vector2d
+{
+    public double X;
+    public double Y;
+
+    public Vector2d(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}
